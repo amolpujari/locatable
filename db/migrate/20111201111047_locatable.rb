8be61ead834ec5e58ea973001f9027f296eabd6e
Amol Pujari <amolpujari@gmail.com>
2012-04-02 21:18:39 +0530

    formatting

--- db/migrate/20111201111047_locatable.rb
+++ db/migrate/20111201111047_locatable.rb
@@ -4,21 +4,20 @@ class Locatable < ActiveRecord::Migration
       t.integer     :locatable_id
       t.string      :locatable_type
 
-			t.string      :ip
-
-			t.string      :country
-			t.string      :state
-			t.string      :state_abbreviation
-			t.string      :county
-			t.string      :city
+      t.string      :ip
+      t.string      :country
+      t.string      :state
+      t.string      :state_abbreviation
+      t.string      :county
+      t.string      :city
       t.string      :street
       t.string      :address
 
-			t.string      :zip
-			t.float       :lat
-			t.float       :lng
+      t.string      :zip
+      t.float       :lat
+      t.float       :lng
       
-			t.timestamps
+      t.timestamps
     end
     
     execute("ALTER TABLE locations MODIFY lat numeric(15,10);")
